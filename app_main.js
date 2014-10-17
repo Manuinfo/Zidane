7209b9c72d9818055765a1f4a2e2f815bda78ec4
z30 <13816025562@139.com>
2014-10-17 22:47:11 +0800

    0.0.4:app_main.js

---
+++ app_main.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes');
+var users = require('./routes/user');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//app.use
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+app.get('/', routes.index);
+app.get('/users', users.list);
+
+
+
+app.listen(3000,function(){
+    console.log('Zidane Web Service is started at 3000');
+});
