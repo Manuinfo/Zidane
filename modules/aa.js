b7bab6a713f96b668c479f1e50ddb42605221e3d
z30 <13816025562@139.com>
2014-10-18 08:36:00 +0800

    0.0.6:db sql modules

---
+++ modules/aa.js
@@ -0,0 +1,11 @@
+/**
+ * Created by z30 on 14-10-18.
+ */
+
+
+var sqls = {
+    'insertSQL': 'insert into t_user(name) values("conan"),("fens.me")',
+    'selectSQL': 'select * from t_user limit 10',
+    'deleteSQL': 'delete from t_user',
+    'updateSQL': 'update t_user set name="conan update"  where name="conan"'
+};
