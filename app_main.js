4ad7925c826ba08626bbc14f83e12043940b6ee2
zacard-orc <13816025562@139.com>
2014-12-06 21:13:46 +0800

    0.1.30:Add Cluster

--- app_main.js
+++ app_main.js
@@ -60,7 +60,7 @@ app.get('/r/2007/:prdname', rest_r.r2007);
 app.get('/r/2008/:qrcode', rest_r.r2008);
 app.get('/r/2009/:rdcode',rest_r.r2009);
 app.get('/r/2010/:nfcid/:qtime',rest_r.r2010);
-
+ 
 app.get('/w/2001/:shopname/:prdname/:place/:price', rest_w.w2001);
 app.get('/w/2002/:prdname/:place/:bcount/:nfccount/:vrftime', rest_w.w2002);
 app.get('/w/2003/:bid/:nfcid', rest_w.w2003);
