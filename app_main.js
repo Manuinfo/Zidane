2372b66e3bc8f4367b826542cfeac777d135561d
13816025562@139.com <love1985b>
2014-12-13 22:19:47 +0800

    0.1.26:Add App Version

--- app_main.js
+++ app_main.js
@@ -134,7 +134,7 @@ conf.Get_ALLAccts(function(confall){ global.u_ACCTS=acc.G_JSON({},confall);});
 
 
 setTimeout(function(){
-    app.listen(4000,function(){
+    app.listen(3000,function(){
         console.log('Zidane Web Service is started at 3000,ID:'+process.pid);
         logger.debug('--------------------------------------------------------')
         logger.debug('Zidane Web Service is started at 3000,ID:'+process.pid);
